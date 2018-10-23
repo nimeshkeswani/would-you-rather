eab1ebceda38816394909cc2137bb9f013325be0
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-10-22 23:17:34 -0700

    get initial data, connect App component and display question ids

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react'
+import { handleInitialData } from '../actions/shared'
+import { connect } from 'react-redux'
+import LoadingBar from 'react-redux-loading'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        App
-      </div>
-    );
-  }
+	componentDidMount() {
+		this.props.dispatch(handleInitialData())
+	}
+	render() {
+		return (
+			<Fragment>
+				<LoadingBar />
+				<div className="App">
+					{
+						this.props.loading === true
+						? null
+						: this.props.questions.map((id) => (<p>{id}</p>))
+					}
+				</div>
+			</Fragment>
+		);
+	}
 }
 
-export default App;
+function mapStateToProps ({ authedUser, questions }) {
+	return {
+		loading: authedUser === null,
+		questions : Object.keys(questions)
+	}
+}
+
+export default connect(mapStateToProps)(App)
