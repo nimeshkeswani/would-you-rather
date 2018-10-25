4e1e49a634b23ffe4c973548564e20edced8afd9
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-10-24 21:34:27 -0700

    display information on the leaderboard page

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -6,7 +6,11 @@ import Nav from './Nav'
 class Leaderboard extends Component {
 	render() {
 
-		if (!this.props.authedUser) {
+		const { authedUser, users } = this.props
+
+		console.log(this.props)
+
+		if (!authedUser) {
 			return <Redirect to='/' />
 		}
 
@@ -14,16 +18,30 @@ class Leaderboard extends Component {
 			<div>
 				<Nav />
 				<div>
-					Leaderboard
+					{users.map((user) => (
+						<div key={user.id}>
+						<img src={user.avatarURL} alt="" height="42" width="42"/> - {user.name} - {user.answeredQuestions} - {user.askedQuestions} - {user.score}
+						</div>
+					))}
 				</div>
 			</div>
 		)
 	}
 }
 
-function mapStateToProps({ authedUser }) {
+function mapStateToProps({ authedUser, users }) {
+
+	let usersInfo = Object.keys(users).map((user) => (users[user])).map((user) => {
+			let userDetail = user
+			userDetail.askedQuestions = userDetail.questions.length
+			userDetail.answeredQuestions = Object.keys(userDetail.answers).length
+			userDetail.score = userDetail.askedQuestions + userDetail.answeredQuestions
+			return userDetail
+		})
+		
 	return {
-		authedUser
+		authedUser,
+		users: usersInfo.sort((a,b) => (a.score > b.score) ? -1 : ((b.score > a.score) ? 1 : 0)),
 	}
 }
 
