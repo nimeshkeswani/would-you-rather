552b15851a76ace39a57e1803f934442b2e7ec2a
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-11-17 19:55:15 -0800

    Updates

---
+++ precache-manifest.d436b4102e89eba805fb65f6c7565444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f522f220959f593645c",
+    "url": "/would-you-rather/static/js/runtime~main.5f522f22.js"
+  },
+  {
+    "revision": "af5ad4d5386bdf9b32d3",
+    "url": "/would-you-rather/static/js/main.af5ad4d5.chunk.js"
+  },
+  {
+    "revision": "6e55902ca7c0f5c3b3d6",
+    "url": "/would-you-rather/static/js/1.6e55902c.chunk.js"
+  },
+  {
+    "revision": "af5ad4d5386bdf9b32d3",
+    "url": "/would-you-rather/static/css/main.81b73a34.chunk.css"
+  },
+  {
+    "revision": "facc9d52cfb3fd4a7dfb90b5e69bcedf",
+    "url": "/would-you-rather/index.html"
+  }
+];
