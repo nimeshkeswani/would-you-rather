196e696040b25a7f370d73d020bc4d12884f6b31
Nimesh Keswani <nimeshkeswani@hotmail.com>
2018-12-24 22:03:54 -0800

    Updates

---
+++ precache-manifest.511bc3e7e19c2574b4dd41abbe85aedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f522f220959f593645c",
+    "url": "/would-you-rather/static/js/runtime~main.5f522f22.js"
+  },
+  {
+    "revision": "2123c1a6088f516f392b",
+    "url": "/would-you-rather/static/js/main.2123c1a6.chunk.js"
+  },
+  {
+    "revision": "32e81ac062393e4d9723",
+    "url": "/would-you-rather/static/js/1.32e81ac0.chunk.js"
+  },
+  {
+    "revision": "2123c1a6088f516f392b",
+    "url": "/would-you-rather/static/css/main.fea8003e.chunk.css"
+  },
+  {
+    "revision": "3d5503b1e138d575d7aebd231897d1d8",
+    "url": "/would-you-rather/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/would-you-rather/precache-manifest.42887aee34d6bcbb715e4e31ccc9f254.js"
+  "/would-you-rather/precache-manifest.511bc3e7e19c2574b4dd41abbe85aedc.js"
 );
 
 workbox.clientsClaim();
